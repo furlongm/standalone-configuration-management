49263cd59b4de4b0d7789f59838f74f833944610
Marcus Furlong <furlongm@gmail.com>
2015-05-06 14:45:07 +0000

    add vim cookbook

---
+++ chef/cookbooks/vim/recipes/default.rb
@@ -0,0 +1,24 @@
+case node['platform_family']
+when 'debian'
+  package ['vim', 'vim-scripts'] do
+    action :install
+  end
+end
+when 'rhel'
+  package 'vim-enhanced' do
+    action :install
+  end
+end
+when 'suse'
+  package ['vim', 'vim-data'] do
+    action :install
+  end
+end
+
+file 'vimrc.local' do
+  path '/etc/vim/vimrc.local'
+  owner 'root'
+  group 'root'
+  mode '0644'
+  action :create
+end
