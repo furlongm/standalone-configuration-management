adef870c9926d7d1f8f659ccff94b677c4002918
Marcus Furlong <furlongm@gmail.com>
2015-06-04 05:57:55 +0000

    use quoted syntax

--- puppet/modules/fail2ban/manifests/init.pp
+++ puppet/modules/fail2ban/manifests/init.pp
@@ -14,8 +14,8 @@ class fail2ban {
 
   file { '/etc/fail2ban/jail.local':
     content => template('fail2ban/jail.local.erb'),
-    owner   => root,
-    group   => root,
+    owner   => 'root',
+    group   => 'root',
     mode    => '0644',
     require => Package['fail2ban'],
     notify  => Service['fail2ban'],
@@ -23,8 +23,8 @@ class fail2ban {
 
   file { '/etc/fail2ban/fail2ban.local':
     source  => 'puppet:///modules/fail2ban/fail2ban.local',
-    owner   => root,
-    group   => root,
+    owner   => 'root',
+    group   => 'root',
     mode    => '0644',
     require => Package['fail2ban'],
     notify  => Service['fail2ban'],
