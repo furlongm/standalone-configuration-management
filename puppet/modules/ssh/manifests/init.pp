ec7ab48143eeced9e75005bfe9223b28e4950d1d
Marcus Furlong <furlongm@gmail.com>
2015-10-30 01:26:45 +0000

    change variable name

--- puppet/modules/ssh/manifests/init.pp
+++ puppet/modules/ssh/manifests/init.pp
@@ -5,17 +5,17 @@ class ssh {
     default  => 'sshd',
   }
 
-  $openssh_server = $::osfamily ? {
+  $openssh_package = $::osfamily ? {
     'Suse'   => 'openssh',
     default => 'openssh-server',
   }
 
-  package { $openssh_server:
+  package { $openssh_package:
     ensure => installed,
   }
 
   service { $openssh_service:
     ensure  => running,
-    require => Package[$openssh_server],
+    require => Package[$openssh_package],
   }
 }
