757286172d11fd0c64e94c58c55b901aee893407
Marcus Furlong <furlongm@gmail.com>
2021-11-03 16:44:27 -0400

    create /etc/puppet dir for puppet

--- puppet/modules/puppet/manifests/init.pp
+++ puppet/modules/puppet/manifests/init.pp
@@ -1,5 +1,12 @@
 class puppet {
 
+  file { '/etc/puppet':
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0755',
+    ensure => directory,
+  }
+
   file { '/etc/puppet/puppet.conf':
     owner  => 'root',
     group  => 'root',
