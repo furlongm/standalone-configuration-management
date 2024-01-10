b5f50d8cdf2fe0acfc0c9736a7b8e290c7a03def
Marcus Furlong <furlongm@gmail.com>
2024-01-09 20:43:35 -0500

    [chef] add locale recipe

---
+++ chef/cookbooks/locale/recipes/default.rb
@@ -0,0 +1,41 @@
+locale = node['locales']['locale']
+timezone = node['locales']['timezone']
+
+package node['locales']['package'] do
+  action :install
+end
+
+if platform_family?('debian')
+  file '/etc/locale.gen' do
+    content  "#{locale} UTF-8\n"
+    mode     '0644'
+    owner    'root'
+    group    'root'
+    notifies :run, 'execute[locale-gen]'
+  end
+
+  template 'environment' do
+    path     '/etc/environment'
+    mode     '0644'
+    owner    'root'
+    group    'root'
+    notifies :run, 'execute[locale-gen]'
+  end
+
+  execute 'locale-gen' do
+    command 'locale-gen'
+    action  :nothing
+  end
+end
+
+execute 'set-locale' do
+  command "localectl set-locale LANG=#{locale}"
+  not_if  { node['virtualization']['system'] == 'docker' }
+  not_if  "localectl status | grep #{locale}"
+end
+
+execute 'set-timezone' do
+  command "timedatectl set-timezone #{timezone}"
+  not_if  { node['virtualization']['system'] == 'docker' }
+  not_if  "timedatectl status | grep #{timezone}"
+end
