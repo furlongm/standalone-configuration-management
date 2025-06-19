f4d32cd630557192e68fb78835df82215feefbbf
Marcus Furlong <furlongm@gmail.com>
2025-06-19 16:38:41 -0400

    add shellcheck fixes and linting job

--- saltstack/standalone-salt.sh
+++ saltstack/standalone-salt.sh
@@ -34,32 +34,33 @@ install_salt() {
 }
 
 install_vim_syntax_highlighting() {
-    if [ ! -z "${SUDO_UID}" ] ; then
-        home=$(getent passwd ${SUDO_UID} | cut -d \: -f 6)
+    if [ -n "${SUDO_UID}" ] ; then
+        home=$(getent passwd "${SUDO_UID}" | cut -d: -f6)
     else
         home=~
     fi
     for i in ftdetect ftplugin indent ; do
-        if [ -f ${home}/.vim/${i}/sls.vim ] ; then
+        if [ -f "${home}"/.vim/${i}/sls.vim ] ; then
             return
         fi
     done
     tmp_dir=$(mktemp -d)
-    mkdir -p ${home}/.vim
-    git clone https://github.com/saltstack/salt-vim ${tmp_dir}
-    cp -r ${tmp_dir}/ftdetect ${tmp_dir}/ftplugin ${tmp_dir}/syntax ${home}/.vim/
-    if [ ! -z "${SUDO_UID}" ] ; then
-        chown -R ${SUDO_UID}:${SUDO_GID} ${home}/.vim
+    mkdir -p "${home}"/.vim
+    git clone https://github.com/saltstack/salt-vim "${tmp_dir}"
+    cp -r "${tmp_dir}"/ftdetect "${tmp_dir}"/ftplugin "${tmp_dir}"/syntax "${home}"/.vim/
+    if [ -n "${SUDO_UID}" ] ; then
+        # shellcheck disable=SC2153
+        chown -R "${SUDO_UID}":"${SUDO_GID}" "${home}"/.vim
     fi
-    rm -fr ${tmp_dir}
+    rm -fr "${tmp_dir}"
 }
 
 get_config_from_github() {
     tmp_dir=$(mktemp -d)
-    git clone --branch ${branch} https://github.com/furlongm/standalone-configuration-management ${tmp_dir}
-    cp -Lr ${tmp_dir}/saltstack/salt /srv
-    cp -r ${tmp_dir}/saltstack/pillar /srv
-    rm -fr ${tmp_dir}
+    git clone --branch "${branch}" https://github.com/furlongm/standalone-configuration-management "${tmp_dir}"
+    cp -Lr "${tmp_dir}"/saltstack/salt /srv
+    cp -r "${tmp_dir}"/saltstack/pillar /srv
+    rm -fr "${tmp_dir}"
     run_path=/srv
 }
 
@@ -74,7 +75,7 @@ main() {
     which patch 1>/dev/null 2>&1 || install_deps
     which salt-call 1>/dev/null 2>&1 || install_salt
     install_vim_syntax_highlighting
-    if [ -z ${run_locally} ] ; then
+    if [ -z "${run_locally}" ] ; then
         get_config_from_github
     else
         get_local_config
