fe96c387e110f187f2f6f3716a32c8abfbe1e350
Marcus Furlong <furlongm@gmail.com>
2025-06-30 23:59:30 -0400

    prefer bind9-dnsutils package on debian

--- chef/cookbooks/net/attributes/default.rb
+++ chef/cookbooks/net/attributes/default.rb
@@ -2,7 +2,7 @@ default['bind-utils']['package'] = 'bind-utils'
 default['iperf']['package'] = 'iperf3'
 
 if platform_family?('debian')
-  default['bind-utils']['package'] = 'dnsutils'
+  default['bind-utils']['package'] = 'bind9-dnsutils'
 end
 
 if platform_family?('suse')
